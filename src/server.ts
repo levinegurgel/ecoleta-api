ec7b0a79f504f2500661e1bce04a6355df172bbc
Levine Gurgel <levinegurgelf@gmail.com>
2020-12-09 08:07:08 -0400

    gerada a estrutura inicial

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    
+    console.log('Listagem de usuários');
+
+    response.send("Hello World");
+
+})
+
+app.listen(3333)
