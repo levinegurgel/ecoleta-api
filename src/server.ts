a1a679ecddcfa4321c10219c32ef029bd357b275
Levine Gurgel <levinegurgelf@gmail.com>
2020-12-10 17:14:13 -0400

    Adicionando rotas em um arquivo separado

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,11 @@
-import express, { request, response } from 'express';
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    'João',
-    'José',
-    'Maria',
-    'Isabel'
-];
-
-app.get('/users', (request, response) => {
-    
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    console.log(filteredUsers);
-    return response.json(filteredUsers);
-});
-
-app.get('/user/:id', (request, response) =>{
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user); 
-});
-
-app.post('/users', (request, response) =>{
-
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333)
