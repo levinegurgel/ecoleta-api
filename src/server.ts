44063b883ac6f497af7a701bfa65f3b62ac03ea7
Levine Gurgel <levinegurgelf@gmail.com>
2020-12-10 15:43:26 -0400

    rota post de criaçao de usuario

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,44 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'João',
+    'José',
+    'Maria',
+    'Isabel'
+];
+
 app.get('/users', (request, response) => {
     
-    response.json([
-        'João',
-        'José',
-        'Maria',
-        'Isabel'
-    ]);
+    const search = String(request.query.search);
 
-    app.post('/users', (request, response) =>{
-        const user = {
-            name: 'João',
-            email: 'joao@gmail.com'
-        };
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
-        return response.json(user);
-    });
+    console.log(filteredUsers);
+    return response.json(filteredUsers);
+});
 
-})
+app.get('/user/:id', (request, response) =>{
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user); 
+});
+
+app.post('/users', (request, response) =>{
+
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+});
 
 app.listen(3333)
